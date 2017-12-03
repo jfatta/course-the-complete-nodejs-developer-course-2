501ae8622ef93dc5bcfa126cd8664a2d7cb5244a
Jorge L. Fatta <jorge.fatta@southworks.net>
2017-12-03 14:58:03 -0300

    using os package

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,9 +1,12 @@
 console.log('Starting app.');
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFile('greetings.txt', 'Hello world!', function(err){
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err){
     if (err){
         console.log('Unable to write to file');
-    }
+    } 
 });
