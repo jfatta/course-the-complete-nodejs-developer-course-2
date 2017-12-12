6e6c5bb1b1747f14f00629a6ea9a9c6b09a639d4
Jorge L. Fatta <jorge.fatta@southworks.net>
2017-12-12 20:50:21 -0300

    cleanup

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,23 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('jorge'));
-var filteredArray = _.uniq(['jorge', 1, 'jorge', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// var res = notes.add(6, -4);
-// console.log('Result:',res);
-// var user = os.userInfo();
-
-
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err){
-//     if (err){
-//         console.log('Unable to write to file');
-//     } 
-// });
