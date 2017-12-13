e16913f5010fe5749e9792e33a90b507b0db84cf
Jorge L. Fatta <jorge.fatta@southworks.net>
2017-12-12 21:45:49 -0300

    use yargs for arguments

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,22 +2,20 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-console.log(process.argv);
-
-var command = process.argv[2];
-console.log('Command: ', command);
+const argv = yargs.argv;
 
 if (command === 'add'){
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list'){
-    console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read'){
-    console.log('Reading note');
+    notes.getNote(argv.title);
 } else if (command === 'remove'){
-    console.log('Removing note');
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
